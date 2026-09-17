319ab4a68e4b71d7f2159230d7e705afb2419a43
Matt Kauffman <matt.kauffman@gmail.com>
2026-09-17 13:41:14 -0700

    chore: regenerate dropdown types for trigger slot

--- types/dropdown/dropdown.d.ts
+++ types/dropdown/dropdown.d.ts
@@ -4,6 +4,7 @@
  * @attr {boolean} open
  * @attr {string} label
  * @slot - Dropdown content
+ * @slot trigger - Optional custom trigger. The default secondary button is used when this slot is empty.
  * @csspart trigger
  * @csspart panel
  * @event rowan-change - Fired when a user toggles or dismisses the dropdown
