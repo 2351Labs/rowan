90f2dbfbc7abdd1010e87e785de33ebcc297fe1a
Matt Kauffman <matt.kauffman@gmail.com>
2026-09-18 08:46:04 -0700

    fix: import Storybook manager API from @storybook/manager-api
    storybook/manager-api is not in this 8.6 exports map, so build-storybook
    failed Quality.

--- .storybook/rowan-theme.js
+++ .storybook/rowan-theme.js
@@ -1,4 +1,4 @@
-import { create, themes } from "@storybook/theming";
+import { create, themes } from "@storybook/theming/create";
 
 /** Manager + Docs chrome. Spread themes.dark so missing keys do not fall back to light. */
 export const rowanManagerTheme = create({
